54ae26e9271d628a8d902a6abfdd02e4f508135e
sonesuke <iamsonesuke@gmail.com>
2021-03-06 21:10:02 +0900

    implement version system

--- mysite/managed_history/models/managed_object.py
+++ mysite/managed_history/models/managed_object.py
@@ -2,16 +2,20 @@ from django.db import models
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from typing import Any
-from .application_object import ApplicationObject
-from .project import Project
 
 
 class ApplicationQuerySet(models.QuerySet):
-    def filter_application(self, application: ApplicationObject) -> models.QuerySet:
+    def filter_application(self, application: Any) -> models.QuerySet:
         return self.filter(version=application.version)
 
-    def filter_project(self, project: Project) -> models.QuerySet:
-        return self.filter(version=project.version())
+    def filter_project(self, project: Any) -> models.QuerySet:
+        return self.filter(project=project, version=project.version())
+
+    def managed_delete(self) -> None:
+        [item.managed_delete() for item in self.all()]
+
+    def managed_save(self) -> None:
+        [item.managed_save() for item in self.all()]
 
 
 class ApplicationQueryManager(models.Manager.from_queryset(ApplicationQuerySet)):  # type: ignore
@@ -34,10 +38,18 @@ class ManagedObject(models.Model):
         newest_version = self.project.version()
         bumping = self.version != newest_version
         if bumping:
-            self.version = newest_version
             self.pk = None
+            self.version = newest_version
         kwargs["force_insert"] = bumping
         super(ManagedObject, self).save(**kwargs)
+
+    def managed_delete(self, **kwargs: Any) -> None:
+        newest_version = self.project.version()
+        bumping = self.version != newest_version
+        if bumping:
+            self.project.bump_version(abandoned_object=self)
+        else:
+            super(ManagedObject, self).delete(**kwargs)
 
 
 @receiver(pre_save, sender=ManagedObject)
--- mysite/managed_history/tests/test_version.py
+++ mysite/managed_history/tests/test_version.py
@@ -8,29 +8,29 @@ class VersionTests(TestCase):
     def test_bump_version(self):
         Project.objects.all().delete()
         project = Project.objects.create()
+        ManagedObject.objects.create(project=project, value="A")
         project.bump_version()
         self.assertEqual(len(project.versions()), 2)
+        self.assertEqual(ManagedObject.objects.filter(project=project).count(), 2)
 
     def test_bump_version_with_object(self):
         Project.objects.all().delete()
         project = Project.objects.create()
-        object = ManagedObject.objects.create(project=project, value="A")
+        ManagedObject.objects.create(project=project, value="A")
         project.bump_version()
-        object.value = "B"
-        object.managed_save()
-        self.assertEqual(ManagedObject.objects.all().count(), 2)
         newest_object = ManagedObject.objects.filter_project(project=project).all()
         self.assertEqual(newest_object.count(), 1)
-        self.assertEqual(newest_object[0].value, "B")
+        newest_object.managed_save()
+        self.assertEqual(ManagedObject.objects.filter_project(project=project).count(), 1)
+        self.assertEqual(ManagedObject.objects.filter(project=project).count(), 2)
 
     def test_managed_delete(self):
         Project.objects.all().delete()
         project = Project.objects.create()
-        object = ManagedObject.objects.create(project=project, value="A")
+        ManagedObject.objects.create(project=project, value="A")
         project.bump_version()
-        object.value = "B"
-        object.managed_save()
-        self.assertEqual(ManagedObject.objects.all().count(), 2)
         newest_object = ManagedObject.objects.filter_project(project=project).all()
-        self.assertEqual(newest_object.count(), 1)
-        self.assertEqual(newest_object[0].value, "B")
+        newest_object.filter(value="A").managed_delete()
+        self.assertEqual(ManagedObject.objects.filter(project=project).count(), 1)
+        newest_object = ManagedObject.objects.filter_project(project=project).all()
+        self.assertEqual(newest_object.count(), 0)
