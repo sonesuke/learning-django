bc0e216eb5142c0dcd8b8c58138e1ffde8688751
sonesuke <iamsonesuke@gmail.com>
2021-03-07 17:47:01 +0900

    refactoring

--- mysite/managed_history/models/historian.py
+++ mysite/managed_history/models/historian.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 from django.db import transaction
-from typing import List, Any
+from typing import List, Any, Optional
+
+
+def _replace_version(managed_object: 'models.ManagedObject', new_version_id: int) -> None:
+    managed_object.pk = None
+    managed_object.version_id = new_version_id
 
 
 class HistorianMixIn:
@@ -10,9 +15,7 @@ class HistorianMixIn:
         return self.project_id  # type: ignore
 
     def version_id(self) -> int:
-        from .history import History
-
-        return History.objects.filter(project_id=self._project_id()).last().version_id  # type: ignore
+        return max(self.version_ids())
 
     def version_ids(self) -> List[int]:
         from .history import History
@@ -30,7 +33,7 @@ class HistorianMixIn:
                 if v not in used_versions_ids:
                     History.objects.filter(project_id=self._project_id(), version_id=v).delete()
 
-    def bump_version(self, abandoned_object: Any = None, force: bool = None) -> int:
+    def bump_version(self, abandoned_object: Optional['models.ManagedObject'] = None, force: bool = None) -> int:
         from .history import History
         from .application_object import ApplicationObject
         from .managed_object import ManagedObject
@@ -40,11 +43,11 @@ class HistorianMixIn:
                 return self.version_id()  # type: ignore
             objects = ManagedObject.objects.filter_project(project=self).all()
             history = History.objects.create(project_id=self._project_id())
+
             for object in objects:
                 if object == abandoned_object:
                     continue
-                object.version_id = history.version_id
-                object.pk = None
+                _replace_version(object, history.version_id)
                 object.save(force_insert=True)
 
             return history.version_id  # type: ignore
@@ -55,7 +58,7 @@ class ApplicationObjectQuerySet(models.QuerySet):
 
 
 class ApplicationObjectManager(models.Manager.from_queryset(ApplicationObjectQuerySet)):  # type: ignore
-    def managed_create(self, project: Any, **kwargs: Any) -> Any:
+    def managed_create(self, project: 'models.Project', **kwargs: Any) -> 'models.ApplicationObject':
         kwargs["project_id"] = project.project_id
         if "version_id" not in kwargs:
             kwargs["version_id"] = project.version_id()
@@ -63,43 +66,39 @@ class ApplicationObjectManager(models.Manager.from_queryset(ApplicationObjectQue
 
 
 class ManagedObjectMixIn:
-    def _project(self, project_id: int) -> HistorianMixIn:
+    def _project(self) -> 'models.Project':
         from .project import Project
 
-        return Project.objects.filter(project_id=project_id).get()  # type: ignore
+        return Project.objects.filter(project_id=self.project_id).get()  # type: ignore
 
     def managed_save(self, **kwargs: Any) -> None:
-        project_id = kwargs["project_id"] if "project_id" in kwargs else self.project_id  # type: ignore
-        newest_version_id = self._project(project_id).bump_version(abandoned_object=self)
-        bumping = self.version_id != newest_version_id  # type: ignore
-        if bumping:
-            self.pk = None
-            self.version_id = newest_version_id  # type: ignore
-        kwargs["force_insert"] = bumping
+        newest_version_id = self._project().bump_version(abandoned_object=self)
+        if self.version_id != newest_version_id:  # type: ignore
+            _replace_version(self, newest_version_id)
         self.save(**kwargs)  # type: ignore
 
     def managed_delete(self, **kwargs: Any) -> None:
-        newest_version_id = self._project(self.project_id).bump_version(abandoned_object=self)  # type: ignore
-        if self.version_id == newest_version_id:
+        newest_version_id = self._project().bump_version(abandoned_object=self)
+        if self.version_id == newest_version_id:  # type: ignore
             self.delete(**kwargs)  # type: ignore
 
 
 class ManagedObjectQuerySet(models.QuerySet):
-    def filter_application(self, application: Any) -> models.QuerySet:
+    def filter_application(self, application: 'models.AppcatlionObject') -> models.QuerySet:
         return self.filter(version_id=application.version_id)
 
-    def filter_project(self, project: Any) -> models.QuerySet:
+    def filter_project(self, project: 'models.Project') -> models.QuerySet:
         return self.filter(project_id=project.project_id, version_id=project.version_id())
 
-    def managed_delete(self) -> None:
-        [item.managed_delete() for item in self.all()]
+    def managed_delete(self, **kwargs: Any) -> None:
+        [item.managed_delete(**kwargs) for item in self.all()]
 
-    def managed_save(self) -> None:
-        [item.managed_save() for item in self.all()]
+    def managed_save(self, **kwargs: Any) -> None:
+        [item.managed_save(**kwargs) for item in self.all()]
 
 
 class ManagedObjectQueryManager(models.Manager.from_queryset(ManagedObjectQuerySet)):  # type: ignore
-    def managed_create(self, project: Any, **kwargs: Any) -> Any:
+    def managed_create(self, project: 'models.Project', **kwargs: Any) -> 'models.ManagedObject':
         kwargs["project_id"] = project.project_id
         if "version_id" not in kwargs:
             kwargs["version_id"] = project.version_id()
